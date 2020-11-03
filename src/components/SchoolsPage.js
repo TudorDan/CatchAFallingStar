59631f42f96ae3d97837a9c3c562d6d231b762c0
TudorDan <tudor.ist@gmail.com>
2020-11-03 16:15:32 +0200

    Add axios functionality

--- src/components/SchoolsPage.js
+++ src/components/SchoolsPage.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Api from "./utils/Api";
 
 const SchoolsPage = () => {
+  const [schools, setSchools] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getSchools = async () => {
+    try {
+      const response = await Api.get("/schools");
+      const schoolsFromAPI = response.data;
+      setSchools(schoolsFromAPI);
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(true);
+    }
+  };
+
+  useEffect(() => {
+    getSchools();
+  }, []);
+
+  // return different statement if api not working
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div className="jumbotron text-center">
-      <h1>Schools LIst</h1>
-      <p>Work in progress.</p>
-    </div>
+    <h1>
+      <h3>Schools</h3>
+      <ul>
+        {schools.map((school) => {
+          const { id, name } = school;
+          return (
+            <li key={id}>
+              <h4>{name}</h4>
+            </li>
+          );
+        })}
+      </ul>
+    </h1>
   );
 };
 
