544c3752907a13518f98ae2a2b37070bb1601a98
TudorDan <tudor.ist@gmail.com>
2021-02-04 15:07:11 +0200

    Refactor footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,65 +1,129 @@
 import React from "react";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFacebookF, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { faMapPin, faEnvelope, faPhoneAlt, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebookF,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faMapPin,
+  faEnvelope,
+  faPhoneAlt,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
 
-library.add(faFacebookF, faTwitter, faYoutube, faMapPin, faEnvelope, faPhoneAlt, faArrowRight)
+library.add(
+  faFacebookF,
+  faTwitter,
+  faYoutube,
+  faMapPin,
+  faEnvelope,
+  faPhoneAlt,
+  faArrowRight
+);
 
 const Footer = () => {
   return (
-    <footer id="footer">
-      <div className="footer-container">
-        <section>
-          <article className="col-1">
-            <h3>Contact</h3>
-            <ul>
-              <li className="address">
-                <a href="/"><FontAwesomeIcon className="map-icon" icon={faMapPin}/>Orsova&nbsp;Street&nbsp;nr.&nbsp;104</a>
-              </li>
-              <li className="email">
-                <a href="/"><FontAwesomeIcon className="email-icon" icon={faEnvelope}/>office@outlook.com</a>
-              </li>
-              <li className="phone last">
-                <a href="/"><FontAwesomeIcon className="phone-icon" icon={faPhoneAlt}/>0213210153</a>
-              </li>
-            </ul>
-          </article>
-          <article className="col-2">
-            <h3>Main Subjects</h3>
-            <ul>
-              <li>
-                <a href="/"><FontAwesomeIcon className="arrow-icon" icon={faArrowRight}/>History</a>
-              </li>
-              <li>
-                <a href="/"><FontAwesomeIcon className="arrow-icon" icon={faArrowRight}/>IT</a>
-              </li>
-              <li>
-                <a href="/"><FontAwesomeIcon className="arrow-icon" icon={faArrowRight}/>Mathematics</a>
-              </li>
-              <li>
-                <a href="/"><FontAwesomeIcon className="arrow-icon" icon={faArrowRight}/>Astronomy</a>
-              </li>
-              <li>
-                <a href="/"><FontAwesomeIcon className="arrow-icon" icon={faArrowRight}/>Geography</a>
-              </li>
-              <li className="last">
-                <a href="/"><FontAwesomeIcon className="arrow-icon" icon={faArrowRight}/>Physics</a>
-              </li>
-            </ul>
-          </article>
-          <article className="col-3">
-            <h3>Social Media</h3>
-            <ul>
-            <li className="facebook"><a href="/"><FontAwesomeIcon className="facebook-icon" icon={faFacebookF}/>Facebook</a></li>
-              <li className="twitter"><a href="/"><FontAwesomeIcon className="twitter-icon" icon={faTwitter}/>Twitter</a></li>
-              <li className="youtube"><a href="/"><FontAwesomeIcon className="youtube-icon" icon={faYoutube}/>YouTube</a></li>
-            </ul>
-          </article>
-        </section>
-        <p className="copyright">2021 &copy; Designed by Coni &amp; Tudor</p>
-      </div>
-    </footer>
+    <>
+      <footer id="footer">
+        <div className="footer-top">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4 col-md-6 footer-contact">
+                <h3>eLearning</h3>
+                <p>
+                  Orșova Street, nr. 104 <br />
+                  Orșova, MH 123432 <br />
+                  România <br />
+                  <br />
+                  <strong>Phone:</strong> +40 123 456 789
+                  <br />
+                  <strong>Email:</strong> office@schoolsystem.com
+                  <br />
+                </p>
+              </div>
+
+              <div className="col-lg-4 col-md-6 footer-links">
+                <h4>Main subjects</h4>
+                <ul>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="/">Hystory</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="/">IT</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="/">Mathematics</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="/">Astronomy</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="/">Geography</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>{" "}
+                    <a href="/">Physics</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-lg-4 col-md-6 footer-newsletter">
+                <h4>Join Our Newsletter</h4>
+                <p>
+                  Tamen quem nulla quae legam multos aute sint culpa legam
+                  noster magna
+                </p>
+                <form action="" method="post">
+                  <input type="email" name="email" />
+                  <input type="submit" value="Subscribe" />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container d-md-flex py-4">
+          <div className="me-md-auto text-center text-md-start">
+            <div className="copyright">
+              &copy; 2021 - Programmed by Coni &amp; Tudor
+            </div>
+            <div className="credits">
+              Graphical template provided by{" "}
+              <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+          </div>
+          <div className="own-social-links text-center text-md-right pt-3 pt-md-0">
+            <a href="/" className="twitter">
+              <i className="bx bxl-twitter"></i>
+            </a>
+            <a href="/" className="facebook">
+              <i className="bx bxl-facebook"></i>
+            </a>
+            <a href="/" className="instagram">
+              <i className="bx bxl-instagram"></i>
+            </a>
+            <a href="/" className="google-plus">
+              <i className="bx bxl-skype"></i>
+            </a>
+            <a href="/" className="linkedin">
+              <i className="bx bxl-linkedin"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+
+      <a href="/" className="back-to-top">
+        <i className="bx bx-up-arrow-alt"></i>
+      </a>
+
+      <div id="preloader"></div>
+    </>
   );
 };
 
