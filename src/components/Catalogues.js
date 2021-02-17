efd779098cd4db9f351502abc62e13e76858ab77
TudorDan <tudor.ist@gmail.com>
2021-02-17 17:41:36 +0200

    Refactor school classes

--- src/components/Catalogues.js
+++ src/components/Catalogues.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import Api from "./utils/Api";
 import Loading from "./utils/Loading";
 import swal from "sweetalert2";
-import { SiGoogleclassroom } from "react-icons/si";
 
 const Catalogues = (school) => {
   const [catalogues, setCatalogues] = useState([]);
@@ -34,7 +33,117 @@ const Catalogues = (school) => {
 
   return (
     <>
-      <h2 className="mt-5 text-center">
+      <section id="features" className="features">
+        <div className="container" data-aos="fade-up">
+          <div className="section-title">
+            <h2>Catalogues</h2>
+            <p>School Classes</p>
+          </div>
+
+          <div className="row" data-aos="zoom-in" data-aos-delay="100">
+            {catalogues.length === 0 ? (
+              <h3 id="loading">No school classes in current school!</h3>
+            ) : (
+              <>
+                {catalogues.map((catalogue) => {
+                  const { id, name } = catalogue;
+
+                  return (
+                    <div key={id} className="col-10 mt-4">
+                      <div className="icon-box">
+                        <i
+                          className="ri-price-tag-2-line"
+                          style={{ color: "#4233ff" }}
+                        ></i>
+
+                        <h3>
+                          <Link
+                            to={{
+                              pathname: linkToCatalogue + id,
+                              schoolData: {
+                                schoolTitle: school.name,
+                              },
+                            }}
+                          >
+                            {name}
+                          </Link>
+                        </h3>
+
+                        <div className="ms-auto">
+                          <Link
+                            to={{
+                              pathname: `/schools/${schoolID}/catalogues/${id}/update`,
+                            }}
+                            className="get-started-btn"
+                          >
+                            Update
+                          </Link>
+                          &nbsp;&nbsp;
+                          <button
+                            className="get-started-btn border-0"
+                            onClick={() => {
+                              swal
+                                .fire({
+                                  title: `Are you sure you wish to delete ${name}?`,
+                                  text: "You won't be able to revert this!",
+                                  icon: "warning",
+                                  showCancelButton: true,
+                                  confirmButtonColor: "#3ec1d5",
+                                  cancelButtonColor: "#3f000f",
+                                  confirmButtonText:
+                                    "Yes, delete school class!",
+                                })
+                                .then(async (result) => {
+                                  if (result.isConfirmed) {
+                                    const response = await Api.delete(
+                                      `/schools/${schoolID}/catalogues/${id}`
+                                    );
+                                    if (response.status === 204) {
+                                      swal
+                                        .fire(
+                                          "Deleted!",
+                                          "Your school class has been deleted.",
+                                          "success"
+                                        )
+                                        .then(function () {
+                                          window.location = `/schools/${schoolID}`;
+                                        });
+                                    }
+                                  }
+                                });
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </>
+            )}
+          </div>
+        </div>
+      </section>
+
+      <div className="why-us">
+        <div className="content text-center">
+          <Link
+            to={{
+              pathname: linkToCatalogue,
+              schoolData: {
+                schoolTitle: school.name,
+              },
+            }}
+            className="more-btn"
+          >
+            Add School Class
+          </Link>
+        </div>
+      </div>
+
+      {/* OLD VERSION BELOW */}
+      {/* <h2 className="mt-5 text-center">
         <span id="secondary-title">School Classes</span>
       </h2>
 
@@ -134,7 +243,7 @@ const Catalogues = (school) => {
             })}
           </>
         )}
-      </ul>
+      </ul> */}
     </>
   );
 };
