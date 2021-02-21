d74c9dd14c1a9c4ec700c5b662bc557f211baddc
TudorDan <tudor.ist@gmail.com>
2021-02-21 20:27:21 +0200

    Refactor catalogue add grade

--- src/components/CatalogueAddGrade.js
+++ src/components/CatalogueAddGrade.js
@@ -5,7 +5,7 @@ import Api from "./utils/Api";
 import swal from "sweetalert";
 import swal2 from "sweetalert2";
 
-const CatalogueAddGrade = () => {
+const CatalogueAddGrade = ({ catalogueName }) => {
   const [catalogueStudents, setCatalogueStudents] = useState([]);
   const [catalogueCourses, setCatalogueCourses] = useState([]);
   const [catalogueMentors, setCatalogueMentors] = useState([]);
@@ -79,168 +79,180 @@ const CatalogueAddGrade = () => {
 
   return (
     <>
-      <h3 className="mt-4">Add new Grade</h3>
-      <div className="card mb-3 mt-5">
-        <Formik
-          className="mt-2"
-          initialValues={{
-            StudentId: "",
-            Mark: "",
-            CourseId: "",
-            MentorId: "",
-            Date: "",
-          }}
-          onSubmit={async (gradeData) => {
-            gradeData.StudentId = parseInt(gradeData.StudentId);
-            gradeData.Mark = parseFloat(gradeData.Mark);
-            gradeData.CourseId = parseInt(gradeData.CourseId);
-            gradeData.MentorId = parseInt(gradeData.MentorId);
+      <section id="contact" className="contact">
+        <div className="container" data-aos="fade-up">
+          <div className="section-title">
+            <h2>Add new course</h2>
+            <p>{catalogueName}</p>
+          </div>
 
-            setLoading(true);
-            try {
-              const response = await Api.post(linkForPost, gradeData);
-              if (response.status === 201) {
-                swal({
-                  title: "Good job!",
-                  text: "Grade was added to the school class",
-                  icon: "success",
-                }).then(function () {
-                  window.location = `/schools/${schoolID}/catalogues/${catalogueId}`;
-                });
-                console.log("success");
-              }
-              const catalogueGradeFromApi = response.data;
-              console.log(catalogueGradeFromApi);
-
-              setLoading(false);
-            } catch (error) {
-              console.log(error.response);
-              const response = error.response;
-
-              if (response.status === 400) {
-                swal2
-                  .fire({
-                    title: `Student or Mentor or Course was not selected!`,
-                    text: "Please choose something for each!",
-                  })
-                  .then(function () {
-                    window.location = `/schools/${schoolID}/catalogues/${catalogueId}`;
-                  });
-              }
+          <Formik
+            className="mt-2"
+            initialValues={{
+              StudentId: "",
+              Mark: "",
+              CourseId: "",
+              MentorId: "",
+              Date: "",
+            }}
+            onSubmit={async (gradeData) => {
+              gradeData.StudentId = parseInt(gradeData.StudentId);
+              gradeData.Mark = parseFloat(gradeData.Mark);
+              gradeData.CourseId = parseInt(gradeData.CourseId);
+              gradeData.MentorId = parseInt(gradeData.MentorId);
 
               setLoading(true);
-            }
-          }}
-        >
-          {(values) => (
-            <Form className="mt-2 ml-3">
-              <div className="form-group row">
-                <label htmlFor="studentId" className="col-sm-2 col-form-label">
-                  Student:
-                </label>
-                <Field
-                  component="select"
-                  name="StudentId"
-                  id="studentId"
-                  className="col-sm-9 form-control"
-                >
-                  {catalogueStudents.map((student) => {
-                    const { id, name } = student;
-                    console.log(id, name);
-                    return (
-                      <option key={id} value={id}>
-                        {name}
-                      </option>
-                    );
-                  })}
-                </Field>
-              </div>
+              try {
+                const response = await Api.post(linkForPost, gradeData);
+                if (response.status === 201) {
+                  swal({
+                    title: "Good job!",
+                    text: "Grade was added to the school class",
+                    icon: "success",
+                  }).then(function () {
+                    window.location = `/schools/${schoolID}/catalogues/${catalogueId}`;
+                  });
+                  console.log("success");
+                }
+                const catalogueGradeFromApi = response.data;
+                console.log(catalogueGradeFromApi);
 
-              <div className="form-group row">
-                <label htmlFor="courseId" className="col-sm-2 col-form-label">
-                  Course:
-                </label>
-                <Field
-                  component="select"
-                  name="CourseId"
-                  id="courseId"
-                  className="col-sm-9 form-control mt-1"
-                >
-                  {catalogueCourses.map((course) => {
-                    const { id, name } = course;
-                    console.log(id, name);
-                    return (
-                      <option key={id} value={id}>
-                        {name}
-                      </option>
-                    );
-                  })}
-                </Field>
-              </div>
+                setLoading(false);
+              } catch (error) {
+                console.log(error.response);
+                const response = error.response;
 
-              <div className="form-group row">
-                <label htmlFor="mentorId" className="col-sm-2 col-form-label">
-                  Mentor:
-                </label>
-                <Field
-                  component="select"
-                  name="MentorId"
-                  id="mentorId"
-                  className="col-sm-9 form-control mt-1"
-                >
-                  {catalogueMentors.map((mentor) => {
-                    const { id, name } = mentor;
-                    console.log(id, name);
-                    return (
-                      <option key={id} value={id}>
-                        {name}
-                      </option>
-                    );
-                  })}
-                </Field>
-              </div>
+                if (response.status === 400) {
+                  swal2
+                    .fire({
+                      title: `Student or Mentor or Course was not selected!`,
+                      text: "Please choose something for each!",
+                    })
+                    .then(function () {
+                      window.location = `/schools/${schoolID}/catalogues/${catalogueId}`;
+                    });
+                }
 
-              <div className="form-group row">
-                <label htmlFor="markDate" className="col-sm-2 col-form-label">
-                  Mark Date:
-                </label>
-                <Field
-                  type="date"
-                  name="Date"
-                  className="col-sm-9 form-control mt-1"
-                  id="markDate"
-                  required
-                />
-              </div>
+                setLoading(true);
+              }
+            }}
+          >
+            {(values) => (
+              <Form className="php-email-form">
+                <div className="form-group d-flex flex-row">
+                  <label
+                    htmlFor="studentId"
+                    className="col-sm-2 col-form-label"
+                  >
+                    Student:
+                  </label>
 
-              <div className="form-group row">
-                <label htmlFor="mark" className="col-sm-2 col-form-label">
-                  Mark:
-                </label>
-                <Field
-                  type="number"
-                  name="Mark"
-                  min="1"
-                  max="10"
-                  step="0.1"
-                  className="col-sm-9 form-control"
-                  id="mark"
-                  placeholder="Please choose a mark"
-                  required
-                />
-              </div>
-              {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
-              <div className="form-group row">
-                <div className="col-sm-12 text-center">
-                  <button type="submit" className="btn custom-btn">
+                  <Field
+                    component="select"
+                    name="StudentId"
+                    id="studentId"
+                    className="w-100 form-control"
+                  >
+                    {catalogueStudents.map((student) => {
+                      const { id, name } = student;
+                      console.log(id, name);
+                      return (
+                        <option key={id} value={id}>
+                          {name}
+                        </option>
+                      );
+                    })}
+                  </Field>
+                </div>
+
+                <div className="form-group d-flex flex-row mt-3">
+                  <label htmlFor="courseId" className="col-sm-2 col-form-label">
+                    Course:
+                  </label>
+
+                  <Field
+                    component="select"
+                    name="CourseId"
+                    id="courseId"
+                    className="w-100 form-control"
+                  >
+                    {catalogueCourses.map((course) => {
+                      const { id, name } = course;
+                      console.log(id, name);
+                      return (
+                        <option key={id} value={id}>
+                          {name}
+                        </option>
+                      );
+                    })}
+                  </Field>
+                </div>
+
+                <div className="form-group d-flex flex-row mt-3">
+                  <label htmlFor="mentorId" className="col-sm-2 col-form-label">
+                    Mentor:
+                  </label>
+
+                  <Field
+                    component="select"
+                    name="MentorId"
+                    id="mentorId"
+                    className="w-100 form-control"
+                  >
+                    {catalogueMentors.map((mentor) => {
+                      const { id, name } = mentor;
+                      console.log(id, name);
+                      return (
+                        <option key={id} value={id}>
+                          {name}
+                        </option>
+                      );
+                    })}
+                  </Field>
+                </div>
+
+                <div className="form-group d-flex flex-row mt-3">
+                  <label htmlFor="markDate" className="col-sm-2 col-form-label">
+                    Mark Date:
+                  </label>
+
+                  <Field
+                    type="date"
+                    name="Date"
+                    className="w-100 form-control"
+                    id="markDate"
+                    required
+                  />
+                </div>
+
+                <div className="form-group d-flex flex-row mt-3">
+                  <label htmlFor="mark" className="col-sm-2 col-form-label">
+                    Mark:
+                  </label>
+
+                  <Field
+                    type="number"
+                    name="Mark"
+                    min="1"
+                    max="10"
+                    step="0.1"
+                    className="w-100 form-control"
+                    id="mark"
+                    placeholder="Please choose a mark"
+                    required
+                  />
+                </div>
+                {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
+                <div className="text-center">
+                  <button type="submit" className="btn-add">
                     Add Grade
                   </button>
                 </div>
-              </div>
-            </Form>
-          )}
-        </Formik>
-      </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </section>
     </>
   );
 };
