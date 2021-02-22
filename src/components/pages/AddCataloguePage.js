48ba5050223ab622e0ae1c11884cb48485c0a44d
TudorDan <tudor.ist@gmail.com>
2021-02-22 15:54:26 +0200

    Refactor add catalogue page

--- src/components/pages/AddCataloguePage.js
+++ src/components/pages/AddCataloguePage.js
@@ -17,80 +17,92 @@ const AddCataloguePage = (props) => {
   }
 
   return (
-    <div className="container school-list text-center">
-      <h1 className="font-weight-bolder" id="school-title">
-        {schoolName}
-      </h1>
-      <div className="underline mb-3"></div>
-      <h3 className="mt-4">Add new Course</h3>
-      <Link to={linkToSchool} className="btn custom-btn">
-        Back to school menu
-      </Link>
+    <>
+      <div className="breadcrumbs" data-aos="fade-in">
+        <div className="container">
+          <h2>{schoolName}</h2>
+          <p>Motto: Audaces fortuna juvat</p>
+        </div>
+      </div>
 
-      <div className="card mb-3 mt-5">
-        <Formik
-          className="mt-2"
-          initialValues={{
-            Name: "",
-            Mentors: [],
-            Students: [],
-            Courses: [],
-            Grades: [],
-          }}
-          onSubmit={async (catalogueData) => {
-            console.log(catalogueData);
+      <section id="contact" className="contact">
+        <div className="container" data-aos="fade-up">
+          <div className="section-title mt-5 mb-5">
+            <h2>School Classes</h2>
+            <p>Add new school class</p>
+          </div>
 
-            setLoading(true);
-            try {
-              const response = await Api.post(linkForPost, catalogueData);
-              if (response.status === 201) {
-                swal({
-                  title: "Good job!",
-                  text: "Your school class was added",
-                  icon: "success",
-                }).then(function () {
-                  window.location = `/schools/${schoolID}`;
-                });
-                console.log("success");
-              }
-              const catalogueFromApi = response.data;
-              console.log(catalogueFromApi);
+          <Formik
+            className="mt-5 mt-lg-0"
+            initialValues={{
+              Name: "",
+              Mentors: [],
+              Students: [],
+              Courses: [],
+              Grades: [],
+            }}
+            onSubmit={async (catalogueData) => {
+              console.log(catalogueData);
 
-              setLoading(false);
-            } catch (error) {
-              console.log(error.response);
               setLoading(true);
-            }
-          }}
-        >
-          {() => (
-            <Form className="mt-2">
-              <div className="form-group row">
-                <label htmlFor="name" className="col-sm-2 col-form-label">
-                  Name:
-                </label>
-                <Field
-                  type="text"
-                  name="Name"
-                  className="col-sm-9 form-control mt-1"
-                  id="name"
-                  placeholder="School Class name"
-                  required
-                />
-              </div>
+              try {
+                const response = await Api.post(linkForPost, catalogueData);
+                if (response.status === 201) {
+                  swal({
+                    title: "Good job!",
+                    text: "Your school class was added",
+                    icon: "success",
+                  }).then(function () {
+                    window.location = `/schools/${schoolID}`;
+                  });
+                  console.log("success");
+                }
+                const catalogueFromApi = response.data;
+                console.log(catalogueFromApi);
 
-              <div className="form-group row">
-                <div className="col-sm-12">
-                  <button type="submit" className="btn custom-btn">
+                setLoading(false);
+              } catch (error) {
+                console.log(error.response);
+                setLoading(true);
+              }
+            }}
+          >
+            {() => (
+              <Form className="php-email-form">
+                <div className="form-group d-flex flex-row">
+                  <label htmlFor="name" className="col-sm-2 col-form-label">
+                    Name:
+                  </label>
+
+                  <Field
+                    type="text"
+                    name="Name"
+                    className="w-100 form-control"
+                    id="name"
+                    placeholder="School Class name"
+                    required
+                  />
+                </div>
+
+                <div className="text-center mt-5 mb-4">
+                  <button type="submit" className="btn-add">
                     Add School Class
                   </button>
                 </div>
-              </div>
-            </Form>
-          )}
-        </Formik>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </section>
+
+      <div className="why-us">
+        <div className="content text-center">
+          <Link to={linkToSchool} className="more-btn">
+            <i className="bx bx-chevron-left"></i> Cancel
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
