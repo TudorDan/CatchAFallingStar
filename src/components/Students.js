7bfeb3f24a297234c8411a773154380450ab29f8
TudorDan <tudor.ist@gmail.com>
2021-03-17 13:47:35 +0200

    Add user roles for school students

--- src/components/Students.js
+++ src/components/Students.js
@@ -5,6 +5,7 @@ import Loading from "./utils/Loading";
 import swal from "sweetalert2";
 import { FaBirthdayCake } from "react-icons/fa";
 import { FaChalkboardTeacher } from "react-icons/fa";
+import { useGlobalUser } from "./utils/AuthContext";
 
 const Students = (school) => {
   const [students, setStudents] = useState([]);
@@ -12,6 +13,7 @@ const Students = (school) => {
   const schoolID = window.location.href.split("/")[4].split("#")[0];
   const linkToAddStudent = `/schools/${schoolID}/persons/1`;
   const apiImgPath = "http://localhost:54719/images/";
+  const { user } = useGlobalUser();
 
   useEffect(() => {
     const getStudents = async () => {
@@ -89,60 +91,67 @@ const Students = (school) => {
                           </p>
                         </div>
 
-                        <div className="trainer d-flex justify-content-around align-items-center">
-                          <div className="trainer-profile d-flex align-items-center">
-                            <Link
-                              to={{
-                                pathname: `${linkToAddStudent}/${id}`,
-                                schoolData: {
-                                  schoolTitle: school.name,
-                                  accessRights: 1,
-                                },
-                              }}
-                              className="get-started-btn"
-                            >
-                              Update
-                            </Link>
-                          </div>
+                        {user.auth &&
+                        (user.roles.includes("admin") ||
+                          user.roles.includes("principal") ||
+                          user.roles.includes("student")) ? (
+                          <div className="trainer d-flex justify-content-around align-items-center">
+                            <div className="trainer-profile d-flex align-items-center">
+                              <Link
+                                to={{
+                                  pathname: `${linkToAddStudent}/${id}`,
+                                  schoolData: {
+                                    schoolTitle: school.name,
+                                    accessRights: 1,
+                                  },
+                                }}
+                                className="get-started-btn"
+                              >
+                                Update
+                              </Link>
+                            </div>
 
-                          <div className="trainer-rank d-flex align-items-center">
-                            <button
-                              className="get-started-btn border-0"
-                              onClick={() => {
-                                swal
-                                  .fire({
-                                    title: `Are you sure you wish to delete ${name}?`,
-                                    text: "You won't be able to revert this!",
-                                    icon: "warning",
-                                    showCancelButton: true,
-                                    confirmButtonColor: "#3ec1d5",
-                                    cancelButtonColor: "#3f000f",
-                                    confirmButtonText: "Yes, delete student!",
-                                  })
-                                  .then(async (result) => {
-                                    if (result.isConfirmed) {
-                                      const response = await Api.delete(
-                                        `/schools/${schoolID}/students/${id}`
-                                      );
-                                      if (response.status === 204) {
-                                        swal
-                                          .fire(
-                                            "Deleted!",
-                                            "Your student has been deleted.",
-                                            "success"
-                                          )
-                                          .then(function () {
-                                            window.location = `/schools/${schoolID}`;
-                                          });
+                            <div className="trainer-rank d-flex align-items-center">
+                              <button
+                                className="get-started-btn border-0"
+                                onClick={() => {
+                                  swal
+                                    .fire({
+                                      title: `Are you sure you wish to delete ${name}?`,
+                                      text: "You won't be able to revert this!",
+                                      icon: "warning",
+                                      showCancelButton: true,
+                                      confirmButtonColor: "#3ec1d5",
+                                      cancelButtonColor: "#3f000f",
+                                      confirmButtonText: "Yes, delete student!",
+                                    })
+                                    .then(async (result) => {
+                                      if (result.isConfirmed) {
+                                        const response = await Api.delete(
+                                          `/schools/${schoolID}/students/${id}`
+                                        );
+                                        if (response.status === 204) {
+                                          swal
+                                            .fire(
+                                              "Deleted!",
+                                              "Your student has been deleted.",
+                                              "success"
+                                            )
+                                            .then(function () {
+                                              window.location = `/schools/${schoolID}`;
+                                            });
+                                        }
                                       }
-                                    }
-                                  });
-                              }}
-                            >
-                              Delete
-                            </button>
+                                    });
+                                }}
+                              >
+                                Delete
+                              </button>
+                            </div>
                           </div>
-                        </div>
+                        ) : (
+                          ""
+                        )}
                       </div>
                     </div>
                   );
@@ -154,11 +163,16 @@ const Students = (school) => {
       </section>
 
       <div className="why-us">
-        <div className="content text-center">
-          <Link to={linkToAddStudent} className="more-btn">
-            Add Student <i className="bx bx-chevron-right"></i>
-          </Link>
-        </div>
+        {user.auth &&
+        (user.roles.includes("admin") || user.roles.includes("principal")) ? (
+          <div className="content text-center">
+            <Link to={linkToAddStudent} className="more-btn">
+              Add Student <i className="bx bx-chevron-right"></i>
+            </Link>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
